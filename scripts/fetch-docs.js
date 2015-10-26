764c454189ea5ce1baab200607250cfdd7d2b07b
Kazunari.NAKATA <o9o64456583@gmail.com>
2015-10-27 00:45:58 +0900

    fix bottom logo

--- scripts/fetch-docs.js
+++ scripts/fetch-docs.js
@@ -40,6 +40,7 @@ function processResult(result) {
     $(this).attr('href', 'http://docs.meteor.com' + $(this).attr('href'));
   });
   $('.logo').attr('src', 'http://docs.meteor.com/logo.png');
+  $('img[src="/logo.png"]').attr('src', 'http://docs.meteor.com/logo.png');
   $('h2, h3, h4').each(function() {
     $(this).attr('id', '/basic/' + $(this).attr('id'));
   });
